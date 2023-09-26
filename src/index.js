3a640fe5b8360634ea973c60a59cf8217451b3c2
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-09-25 21:46:44 -0300

    fix: bud on read

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const path = require('path');
 const fs = require('fs');
 
 const barrel = {};
+const BASE_PATH = path.resolve(path.basename(__dirname))
 
-const folders = fs.readdirSync(path.resolve(__dirname))
+const folders = fs.readdirSync(BASE_PATH)
   .filter((item) => !item.includes('.js'));
 
 folders.forEach((folder) => {
-  const files = fs.readdirSync(path.resolve(__dirname, folder));
-  console.log(files);
+  const files = fs.readdirSync(path.resolve(BASE_PATH, folder));
   const folderName = folder.charAt(0).toUpperCase() + folder.slice(1);
   files.forEach((file) => {
-    const requiredObj = require(path.resolve(__dirname, folder, file));
+    const requiredObj = require(path.resolve(BASE_PATH, folder, file));
     const name = requiredObj.name;
     delete requiredObj.name;
     barrel[folderName] = {
